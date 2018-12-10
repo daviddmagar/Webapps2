d8c30a64b04ad32e223569b60d88e6d3011645a6
robodude2829 <ddm2084@utulsa.edu>
2018-12-10 03:58:23 -0600

    Added a procfile

--- cart/view.mjs
+++ cart/view.mjs
@@ -8,14 +8,12 @@
     <!-- Required meta tags, both charset and viewport -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-
     <!-- Bootstrap CSS, comes from the Bootstrap starter tempalte -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/style.css">
     <!-- JQuery first, then Popper.js, then Bootstrap JS, comes from the Bootstrap starter template -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script></link>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
         crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
@@ -30,7 +28,7 @@
 
 
     <!-- Title of the project -->
-    <title>Zmazon Online Marketplace</title>
+    <title>Zmazon Online Checkout</title>
 
 <!--Shamelessly ripped from the bootstrap site cause I couldn't get expand to work with my project 1 nav bar for some reason.-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
